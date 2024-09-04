6cf2a93b262c31b3f1a959204291076c132cbd70
Teodora217 <teodoraivanova007@abv.bg>
2024-09-04 21:59:59 +0300

    Add Login and Register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/Header"
 import Home from "./components/Home"
 import GameLists from "./components/GameLists"
 import CreateGame from "./components/CreateGame"
+import Login from "./components/Login"
+import Register from "./components/Register"
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/gamelists" element={<GameLists/>}/>
       <Route path="/create" element={<CreateGame/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
     </Routes>
     <Home/>
     </div>
