5585574c581d6d911946a39c3f9bc38a69255f8b
Teodora217 <teodoraivanova007@abv.bg>
2024-09-04 21:52:29 +0300

    Add CreateGame component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Home from "./components/Home"
 import GameLists from "./components/GameLists"
+import CreateGame from "./components/CreateGame"
+
+
 function App() {
 
   return (
@@ -10,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/gamelists" element={<GameLists/>}/>
+      <Route path="/create" element={<CreateGame/>}/>
     </Routes>
     <Home/>
     </div>
