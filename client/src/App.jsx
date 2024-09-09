c928d01e7d5be7967344605df52aa5b1cc87bb9c
Teodora217 <teodoraivanova007@abv.bg>
2024-09-09 23:47:08 +0300

    Made logout and authorized request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { Routes, Route, useNavigate } from "react-router-dom"
 import * as authService from './services/authService'
 import AuthContext from "./contexts/authContext"
+import Path from "../src/paths"
 import Header from "./components/Header"
 import Home from "./components/Home"
 import GameLists from "./components/GameLists"
@@ -9,17 +10,21 @@ import CreateGame from "./components/CreateGame"
 import Register from "./components/Register"
 import Login from "./components/Login"
 import GameDetails from "./components/GameDetails"
-import Path from "../src/paths"
-
-
+import Logout from "./components/Logout"
 
 function App() {
 const navigate = useNavigate();
-const [auth, setAuth] = useState({});
+const [auth, setAuth] = useState(() => {
+  localStorage.removeItem('accessToken');
+
+  return {};
+
+});
 
   const loginSubmitHandler = async (values )=> {
   const result = await authService.login(values.email, values.password);
   setAuth(result)
+  localStorage.setItem('accessToken', result.accessToken);
   navigate(Path.Home)
   };
 
@@ -29,9 +34,15 @@ const [auth, setAuth] = useState({});
     navigate(Path.Home);
   } 
 
+  const logoutHandler = () => {
+    setAuth({});
+    localStorage.removeItem('accessToken');
+  }
+
   const values = {
     loginSubmitHandler,
     registerSubmitHandler,
+    logoutHandler,
     username: auth.username || auth.email,
     email: auth.email,
     isAuthenticated: !!auth.email, // convert to boolean value
@@ -48,6 +59,7 @@ const [auth, setAuth] = useState({});
       <Route path="login" element={<Login />}/>
       <Route path="/register" element={<Register/>}/>
       <Route path="/games/:gameId" element={<GameDetails/>}/>
+      <Route path={Path.Logout} element={<Logout/>}/>
     </Routes>
     <Home/>
     </div>
--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -7,6 +7,14 @@ const buildOptions = (data) => {
         'content-type': 'application/json'
     };
  }
+
+ const token = localStorage.getItem('accessToken')
+ if(token) {
+  options.headers = {
+    ...options.headers,
+    'X-Authorization': token
+  };
+ }
  return options;
 }
 
@@ -16,8 +24,15 @@ const request = async (method, url, data) => {
       method,
     });
   
+    if(response.status === '204') {
+      return {};
+    }
     const result = await response.json();
   
+    if(!response.ok) {
+      throw result;
+    }
+
     return result;
   };
 
